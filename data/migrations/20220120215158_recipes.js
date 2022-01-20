f0d0cfed186127689c0c349b7e6686b9ce6fe3ee
abc <abc@example.com>
2022-01-20 15:15:39 -0700

    working migrations;

---
+++ data/migrations/20220120215158_recipes.js
@@ -0,0 +1,51 @@
+
+exports.up = async function(knex) {
+  await knex.schema
+    .createTable('recipes', table => {
+        table.increments('recipe_id')
+        table.string('recipe_name', 128).notNullable().unique()
+        table.datetime('created_at')
+    })
+    .createTable('steps', table => {
+        table.increments('step_id')
+        table.integer('step_number').notNullable()
+        table.string('step_instruction').notNullable()
+        table.integer('recipe_id')
+            .unsigned()
+            .notNullable()
+            .references('recipe_id')
+            .inTable('recipes')
+            .onDelete('CASCADE')
+            .onUpdate('CASCADE')
+    })
+    .createTable('ingredients', table => {
+        table.increments('ingredient_id')
+        table.text('ingredient_name').notNullable()
+    })
+    .createTable('r_s_i', table => {
+        table.increments('r_s_i_id')
+        table.integer('step_id')
+            .unsigned()
+            .notNullable()
+            .references('step_id')
+            .inTable('steps')
+            .onDelete('CASCADE')
+            .onUpdate('CASCADE')
+        table.integer('ingredient_id')
+            .unsigned()
+            .notNullable()
+            .references('ingredient_id')
+            .inTable('ingredients')
+            .onDelete('CASCADE')
+            .onUpdate('CASCADE')
+        table.float('ingredient_amount').notNullable()
+    })
+};
+
+exports.down = async function(knex) {
+  await knex.schema
+    .dropTableIfExists('r_s_i')
+    .dropTableIfExists('ingredients')
+    .dropTableIfExists('steps')
+    .dropTableIfExists('recipes')
+};
